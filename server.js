040c4ffc0dc4438dfaa1680ba768e7955e6a239c
Sahil Shirsath <sahilshirsath75.com>
2025-11-03 04:00:57 +0530

    user dashbaord cors resolved???

--- server.js
+++ server.js
@@ -25,9 +25,13 @@ const app = express();
 
 connectDB();
 
-app.use(helmet());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000","https://government-frontend-steel.vercel.app,","https://government-user-frontend.vercel.app/"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000", 
+    "https://government-frontend-steel.vercel.app",
+    "https://government-user-frontend.vercel.app"
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
